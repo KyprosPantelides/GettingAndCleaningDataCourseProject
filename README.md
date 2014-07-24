39cd778f7d50597d04d40a5578bb04d43215fa7d
Kypros <kyprianos@pantelides.eu>
2014-07-24 19:52:33 +0300

    reworking

--- README.md
+++ README.md
@@ -5,18 +5,13 @@ date: "Wednesday, July 23, 2014"
 output: html_document
 ---
 
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+the readme describes things like
 
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-```{r}
-summary(cars)
-```
-
-You can also embed plots, for example:
-
-```{r, echo=FALSE}
-plot(cars)
-```
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+    listing all the related files, 
+    instructions on how to use the script, 
+    a general description of the raw data and it's source, 
+    a description of what the script will do
+    
+    The Readme explains where the data comes from, how the data has been manipulated, what are the outputs of the process.
+    
+    
--- run_analysis.R
+++ run_analysis.R
@@ -1,57 +1,73 @@
-#       Coursera Getting and Cleaning Data Course Project
-#       
-#       This script file "run_analysis.R", that can be run as long as the Samsung data is in the working directory,
-#       will perform the following steps:
+#       Coursera - Getting and Cleaning Data Course Project
+       
+#       This script file "run_analysis.R" will perform the following steps, as per project requirement:
 #       1.Merges the training and the test sets to create one data set.
 #       2.Extracts only the measurements on the mean and standard deviation for each measurement.
 #       3.Uses descriptive activity names to name the activities in the data set
 #       4.Appropriately labels the data set with descriptive variable names.
-#       5.Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-#
-#       First, we read in the data from files. 
+#       5.Creates a second, independent tidy data set with the average of each variable
+#	 for each activity and each subject. 
 
+#	The working directory is the unzipped UCI_HAR_Dataset folder. 
+
+#       Read the measurment and activity names data from files. 
 features = read.table("features.txt")
 activityLabels  = read.table("activity_labels.txt")
-# convert variable names to lowercase
+
+# 	Convert activity names to lower case
 activityLabels$V2 = tolower(activityLabels$V2)
 
-
+#       Read the Test data from files.
 subjectTest<-read.table("test\\subject_test.txt")   
 XTest<-read.table("test\\X_test.txt")
 yTest<-read.table("test\\y_test.txt")
-#activityTest<-merge(yTest,activityLabels,by.x="V1",by.y="V1",all=TRUE)
 
+#	Assign column names to the Test data 
 colnames(subjectTest) = "subjectId"
 colnames(yTest) = "activity"
 colnames(XTest) = features[,2]
 
+#       Read the Train data from files.
 subjectTrain<-read.table("train\\subject_train.txt")
 XTrain<-read.table("train\\X_train.txt")
 yTrain<-read.table("train\\y_train.txt")
 
+#	Assign column names to the Train data 
 colnames(subjectTrain) = "subjectId"
 colnames(yTrain) = "activity"
 colnames(XTrain) = features[,2]
 
+#	Add all the columns of the 3 tables of Train data in a new table. 
 trainSet = cbind(subjectTrain, yTrain, XTrain)
+
+#	Add all the columns of the 3 tables of Test data in a new table. 
 testSet = cbind(subjectTest, yTest, XTest)
+
+#	Merge all the rows of the new Test and Train tables in a new full data table. 
 fullSet = rbind(testSet, trainSet)
 
+#	Extract only the measurements on the mean and standard deviation in a new table
 extractSet<-fullSet[,grep("subjectId|activity|mean|std",colnames(fullSet))] 
 
+#	Add descriptive activity names to name the activities in the data set 
+#	by merging the sets, rearranging the new added column and deleting the unneeded
 extractSet = merge(extractSet,activityLabels,by.x="activity",by.y="V1",all=TRUE)
 extractSet$activity<-extractSet$V2
 extractSet<-extractSet[,1:81]
 
-# tidy up variable names
+# 	Tide up the variable names. 
 names(extractSet) <- gsub(pattern="^t",replacement="time",x=names(extractSet))
 names(extractSet) <- gsub(pattern="^f",replacement="freq",x=names(extractSet))
 names(extractSet) <- gsub(pattern="-mean",replacement="Mean",x=names(extractSet))
 names(extractSet) <- gsub(pattern="-std",replacement="StdDev",x=names(extractSet))
 names(extractSet) <- gsub(pattern="BodyBody",replacement="Body",x=names(extractSet))
 names(extractSet) <- gsub(pattern="\\()",replacement="",x=names(extractSet))
+names(extractSet) <- gsub(pattern="-",replacement="",x=names(extractSet))
 
+#	Creates a tidy data set with the average of each variable for each activity 
+#	and each subject. library(plyr) should be loaded, if not already.
+library(plyr)
 tidyData = ddply(extractSet, .(activity, subjectId), numcolwise(mean))
 
-# Export the tidyData set 
-write.table(tidyData, file="tidyData.txt")
+# 	Export the tidy Data set 
+write.table(tidyData, file="tidyDataSet.txt")
